c6818c6628e31fbe8fba37fc3edb314cec384afb
Iván David Barrón Báez <ivandavid77@gmail.com>
2015-06-16 16:50:22 -0500

    debug

--- test_all.php
+++ test_all.php
@@ -28,6 +28,5 @@
     require_once('image_utils.php');
     $source = dirname(__FILE__).'/test_image_source.jpg';
     $target = dirname(__FILE__).'/test_image_target.jpg';
-    echo $source;
-    echo $target;
     resize_image($source, 200, 150, $target);
+    header('Location: test_image_target.jpg');
